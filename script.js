4cb6aea513d51cb17c9cb5452c7755970fb44d45
Utkarsh Dayal <uxd2486@rit.edu>
2022-06-27 19:15:29 -0400

    Added button to change the number of squares in the canvas

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function makeCanvas(size) {
     const canvas = document.querySelector(".canvas");
-    for (let i = 0; i < size / 2; i++) {
+    for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        for (let j = 0; j < size / 2; j++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
             square.addEventListener("mouseover", changeColour);
@@ -16,6 +16,16 @@ function makeCanvas(size) {
 function changeColour(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     this.style.backgroundColor = "#" + randomColor;
+}
+
+function changeCanvasSize(){
+    let size = parseInt(prompt("Enter new size:", "16"));
+    const canvas = document.querySelector(".canvas");
+    const children = canvas.children;
+    while (children.item(0) != null){
+        canvas.removeChild(children.item(0));
+    }
+    makeCanvas(size);
 }
 
 DEFAULT_SIZE = 16;
